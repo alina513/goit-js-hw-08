3acae7e72cdc6ef1e1030ca6ee1ac5d6be1c24fb
alina513 <130853290+alina513@users.noreply.github.com>
2023-09-04 01:10:51 +0300

    end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time" , JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(currentTime);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+
+import throttle from "lodash.throttle";
+const form = document.querySelector(".feedback-form");
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+update();
+
+form.addEventListener('input', onInput);
+form.addEventListener("submit", throttle(onSubmit, 500));
+
+function onInput(event) {
+    let locStorage = {
+        message: event.currentTarget.elements.message.value,
+        email: event.currentTarget.elements.email.value
+    };
+    let stringifyLocal = JSON.stringify(locStorage);
+    localStorage.setItem(LOCALSTORAGE_KEY, stringifyLocal);
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+   localStorage.removeItem(LOCALSTORAGE_KEY);
+   form.reset();
+};
+   
+
+function update() {
+    let messageParse = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    let messageCuurent = messageParse ? messageParse.message : "";
+    let emailCurrent = messageParse ? messageParse.email : "";
+    form.message.value = messageCuurent;
+    form.email.value = emailCurrent; 
+}
